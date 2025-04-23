cd5b998e52d3cff8730236add662ecfee1c3507b
Sliftlol <127846994+Sliftlol@users.noreply.github.com>
2025-04-23 20:17:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ addEventListener('fetch', event => {
 
 // Construct request with original URI
 async function handleRequest(event) {
-	const TS = "https://www.slift.space/", 
-	const WORKER_ENDPOINT = "https://worker.bugbounty208.workers.dev/" 
+  const TS = "https://www.slift.space/", 
+  const WORKER_ENDPOINT = "https://worker.bugbounty208.workers.dev/" 
   const request = event.request
   const path = request.url.replace(WORKER_ENDPOINT,"")
   const destUrl = TS + path               
